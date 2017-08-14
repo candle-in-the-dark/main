c6f6adc74eb8c1ef6161eff7bcc3d9011574ba28
donkeypagoda <christopherallenstewart@gmail.com>
2017-08-14 14:38:24 -0700

    fucking everything up

--- public/js/maze.js
+++ public/js/maze.js
@@ -1,88 +1,3 @@
-// map.layers = Json return
-
-var map = {
-    cols: 12,
-    rows: 12,
-    tsize: 64,
-    layers: [[
-        3, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
-        3, 1, 1, 1, 1, 3, 1, 1, 1, 3, 1, 3,
-        3, 3, 3, 3, 1, 1, 1, 3, 1, 3, 1, 1,
-        3, 1, 3, 1, 1, 3, 3, 3, 1, 3, 1, 3,
-        3, 1, 3, 1, 3, 3, 1, 1, 1, 3, 1, 3,
-        3, 1, 3, 1, 1, 3, 1, 3, 3, 3, 1, 3,
-        3, 1, 3, 3, 1, 3, 1, 1, 1, 1, 1, 3,
-        3, 1, 1, 1, 1, 3, 3, 3, 1, 3, 1, 3,
-        3, 1, 3, 1, 1, 1, 1, 3, 1, 3, 1, 3,
-        3, 1, 3, 3, 3, 1, 3, 3, 3, 3, 1, 3,
-        3, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 3,
-        3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3
-    ], [
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-    ], [
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-    ]],
-    getTile: function (layer, col, row) {
-        return this.layers[layer][row * map.cols + col];
-    },
-    isSolidTileAtXY: function (x, y) {
-        var col = Math.floor(x / this.tsize);
-        var row = Math.floor(y / this.tsize);
-
-        // tiles 3 and 5 are solid -- the rest are walkable
-        // loop through all layers and return TRUE if any tile is solid
-        return this.layers.reduce(function (res, layer, index) {
-            var tile = this.getTile(index, col, row);
-            var isSolid = tile === 3 || tile === 5;
-            return res || isSolid;
-        }.bind(this), false);
-    },
-    isAtEnd: function (x, y) {
-        let col = Math.floor(x / this.tsize);
-        let row = Math.floor(y / this.tsize);
-        // tile 1 is the end, other tile values can be added later for other tasks/easter eggs
-        return this.layers.reduce(function(acc, layer, index) {
-          let tile = this.getTile(index, col, row);
-          let isEnd = tile === 6;
-          return acc || isEnd;
-        }.bind(this), false);
-    },
-    getCol: function (x) {
-        return Math.floor(x / this.tsize);
-    },
-    getRow: function (y) {
-        return Math.floor(y / this.tsize);
-    },
-    getX: function (col) {
-        return col * this.tsize;
-    },
-    getY: function (row) {
-        return row * this.tsize;
-    }
-};
-
 function Camera(map, width, height, radius) {
     this.x = 0;
     this.y = 0;
@@ -204,7 +119,7 @@ Game.load = function () {
     ];
 };
 
-Game.init = function () {
+Game.init = function (map) {
     Keyboard.listenForEvents(
         [Keyboard.LEFT, Keyboard.RIGHT, Keyboard.UP, Keyboard.DOWN]);
     this.tileAtlas = Loader.getImage('tiles');
