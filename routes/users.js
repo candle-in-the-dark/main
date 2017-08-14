ec533a5692f6dfcfcb9b2857b878a342e149f0d5
John York-Erwin <solidseussian@gmail.com>
2017-08-14 15:41:30 -0700

    did work on dedAndWin html and CSS

--- routes/users.js
+++ routes/users.js
@@ -11,9 +11,8 @@ const router = express.Router();
 router.post('/users', (req, res, next) => {
   knex('users').where('email', req.body.email).first()
     .then((row) => {
-      console.log(row)
       if (row) {
-        return next(boom.create(200, 'Email is already registered to a user.'));
+        return next(boom.create(400, 'Email is already registered to a user.'));
       }
       bcrypt.hash(req.body.password, 12)
         .then((password) => {
