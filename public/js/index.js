0411dd216070d53fcb224499a4481124802c61da
donkeypagoda <christopherallenstewart@gmail.com>
2017-08-14 17:20:51 -0700

    tweaking some stuff

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,5 @@
 let numbOfMaps = 3;
 $('#guestPlay').on('click', () => {
-  let choice = Math.floor(((Math.random() * 10) + 1) / numbOfMaps)
+  let choice = Math.floor((Math.random()* 10) / numbOfMaps)
   window.location.href = `../maze.html?mapId=${choice}`
 })
