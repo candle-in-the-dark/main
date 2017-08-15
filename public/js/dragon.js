8c63b0fe17678e8e7ea53d5108ecfd30f9a42d14
donkeypagoda <christopherallenstewart@gmail.com>
2017-08-15 14:32:37 -0700

    got some trivia happening

--- public/js/dragon.js
+++ public/js/dragon.js
@@ -1,3 +1,6 @@
+const questionDiv = $("#question");
+const heading = $("<h5>");
+
 function getRiddle(){
 const url = 'https://opentdb.com/api.php?amount=10&category=20&difficulty=medium&type=multiple';
 const xhr = $.getJSON(url);
@@ -5,9 +8,15 @@ xhr.done(function(data){
   if (xhr.status !== 200){
     return;
   }
-  console.log(data);
+  let choice = Math.floor(Math.random() * 10);
+  console.log(data.results[choice]);
+  let question = data.results[choice].question
+  heading.text(question);
+  questionDiv.append(heading);
+  // question.text(data);
 });
 };
+
 window.onload = function() {
   getRiddle();
 }
