ce7e88ab68cb1456e9a092f16c107fc3d49d0a7d
donkeypagoda <christopherallenstewart@gmail.com>
2017-08-16 12:45:03 -0700

    fixed ded page image

--- public/js/scoreboard.js
+++ public/js/scoreboard.js
@@ -12,6 +12,7 @@ $.ajax(options)
       const mapId = $('<td>').text(element.map_id);
       const username = $('<td>').text(element.username);
       const score = $('<td>').text(element.score);
+      // const quest =
       $('#singlePlay').append(row.append(mapId).append(username).append(score));
     })
   })
