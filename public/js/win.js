d90bc9024f659d9a9b56f6ab0d70783a76f6f673
donkeypagoda <christopherallenstewart@gmail.com>
2017-08-17 11:14:30 -0700

    working

--- public/js/win.js
+++ public/js/win.js
@@ -1,19 +1,19 @@
-const info = JSON.parse(localStorage.getItem('info'));
-const score = info.questScore;
+// const info = JSON.parse(localStorage.getItem('info'));
+// const score = info.questScore;
 
-const options = {
+const questGet = {
   contentType: 'application/json',
   dataType: 'json',
   type: 'GET',
   url: `/usernames/scores/quest/win`
 }
 
-$.ajax(options)
+$.ajax(questGet)
   .then((result) => {
-    result.forEach((element) => {
-      const username = element.username;
-      $('#user').text(`Congratulations ${username}!!`)
-      $('#questScore').text (`You had ${score} seconds remaining when you completed the Quest!`);
-    })
+    const username = result.username;
+    const questscore = result.questscore;
+    $('#user').text(`Congratulations ${username}!!`)
+    $('#questScore').text (`You had ${score} seconds remaining when you completed the Quest!`);
+
   })
   .catch((err) => { });
