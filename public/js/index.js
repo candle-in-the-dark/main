efd6bfbd48e21e086369a9cd4ee71834cedfc3d3
John York-Erwin <solidseussian@gmail.com>
2017-08-17 15:08:45 -0700

    fixed many bugs

---
+++ public/js/index.js
@@ -0,0 +1,6 @@
+$('#guestPlay').on('click', () => {
+  let numbOfMaps = 3;
+  let choice = Math.floor(Math.random() * numbOfMaps) + 1;
+  localStorage.setItem('info', JSON.stringify({'mapScore': 0, 'inQuest': false, 'questScore':0}));
+  window.location.href = `../maze.html?mapId=${choice}`;
+})
