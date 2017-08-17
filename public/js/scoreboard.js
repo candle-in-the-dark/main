924d7f71a0ba96b9d75607c3f21f87c78eeb7e8a
John York-Erwin <solidseussian@gmail.com>
2017-08-17 11:03:59 -0700

    rdid scorepage

--- public/js/scoreboard.js
+++ public/js/scoreboard.js
@@ -1,24 +1,53 @@
-const options = {
-  contentType: 'application/json',
-  dataType: 'json',
-  type: 'GET',
-  url: '/usernames/scores'
+const getMaps = {
+    contentType: 'application/json',
+    dataType: 'json',
+    type: 'GET',
+    url: '/maps'
+}
+$.ajax(getMaps)
+.then((result) => {
+  renderButtons(result);
+})
+.catch((err) => {})
+
+function renderButtons(mapArray) {
+  mapArray.forEach(function(element) {
+    const id = parseInt(element.id)
+    const button = $('<a>').addClass('button').attr('id', id).text(`Map ${id}`)
+    button.on('click', (() => {
+      $('#title').text(`Scores for map ${id}`);
+      renderScores(id);
+    }));
+    $('#buttons').append(button)
+  })
 }
 
-$.ajax(options)
+
+function renderScores(mapId) {
+  const options = {
+    contentType: 'application/json',
+    dataType: 'json',
+    type: 'GET',
+  }
+  if (mapId === 'quest') {
+    options.url = '/usernames/scores/quest'
+  } else {
+    options.url = `/usernames/scores/${mapId}`
+  }
+  $.ajax(options)
   .then((result) => {
     result.sort(function (a, b){
       return b.score - a.score;
     });
-    console.log(result)
+    //put this down here to avoid a flicker effect between asynchronous calls
+    $('#scores').empty();
     result.forEach((element) => {
       const row = $('<tr>');
-      const mapId = $('<td>').text(element.map_id);
       const username = $('<td>').text(element.username);
       const score = $('<td>').text(element.score);
-      const quest = $('<td>').text(element.quest);
 
-      $('#scores').append(row.append(mapId).append(username).append(score).append(quest));
+      $('#scores').append(row.append(username).append(score));
     })
   })
   .catch((err) => { });
+}
