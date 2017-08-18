0b021e5e1d8b589acb19348b60d6b173284831ce
donkeypagoda <christopherallenstewart@gmail.com>
2017-08-17 17:28:21 -0700

    tweaking

--- public/js/scoreboard.js
+++ public/js/scoreboard.js
@@ -41,7 +41,6 @@ function renderButtons(mapArray) {
   })
 }
 
-
 function renderScores(mapId) {
   const options = {
     contentType: 'application/json',
@@ -76,3 +75,50 @@ function renderScores(mapId) {
   })
   .catch((err) => { });
 }
+
+const logCheck = {
+  contentType: 'application/json',
+  dataType: 'json',
+  type: 'GET',
+  url: '/token'
+}
+const logOut = {
+  type: 'DELETE',
+  url: '/token'
+}
+
+const logOutButton = $("#logout")
+const login = $("#login")
+const register = $("#register")
+const guestPlay = $("#guestPlay")
+
+
+$.ajax(logCheck)
+.then((check) => {
+  if (check === false) {
+    logOutButton.addClass("hidden")
+  }
+  else{
+    logOutButton.removeClass("hidden")
+    login.addClass("hidden")
+    register.addClass("hidden")
+  }
+})
+.catch((err) => console.log(err))
+
+$("#logout").on("click", () => {
+  $.ajax(logOut)
+    .then(() => {
+      logOutButton.addClass("hidden")
+      login.removeClass("hidden")
+      register.removeClass("hidden")
+    })
+    .catch((err) => console.log(typeof err))
+})
+
+$('#guestPlay').on('click', () => {
+  let numbOfMaps = 3;
+  let choice = Math.floor(Math.random() * numbOfMaps) + 1;
+  localStorage.setItem('info', JSON.stringify({'mapScore': 0, 'inQuest': false, 'questScore':0}));
+  window.location.href = `../maze.html?mapId=${choice}`;
+})
